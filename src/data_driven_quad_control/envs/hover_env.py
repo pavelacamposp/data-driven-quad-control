Ensure correct quaternion transformation in drone env
Update `quat_to_xyz` usage in `HoverEnv` to include `rpy=True` and
`degrees=True` parameters, aligning with the `hover_env.py` module
from Genesis-Embodied-AI/Genesis@7c72c4b.

src/data_driven_quad_control/envs/hover_env.py
@@ -373,7 +373,9 @@ class HoverEnv:
             transform_quat_by_quat(
                 torch.ones_like(self.base_quat) * self.inv_base_init_quat,
                 self.base_quat,
-            )
+            ),
+            rpy=True,
+            degrees=True,
         )
         inv_base_quat = inv_quat(self.base_quat)
         self.base_lin_vel[:] = transform_by_quat(