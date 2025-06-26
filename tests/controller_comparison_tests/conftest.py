0b6672714f5d8a1f59662357f03913937d8fa6cb
Pável A. Campos-Peña <pcamposp@uni.pe>
2025-06-26 03:20:51 -0500

    Simplify controller comparison integration test
    - Add a data-driven MPC controller configuration file
      (`test_dd_mpc_params.yaml`) to
      `tests/controller_comparison_tests/config/data_driven_mpc/` to
      reduce the complexity of the controller comparison integration
      test.
    - Add a test RL model (`test_ctbr_fixed_yaw_model.pt`) to
      `tests/controller_comparison_tests/models/`.
    - Update paths for the `dd_mpc_controller` and `rl_ppo_model`
      controller configurations in
      `test_controller_comparison_config.yaml`.
    - Move `test_controller_comparison_config.yaml` from
      `tests/controller_comparison_tests/config` to
      `tests/controller_comparison_tests/config/comparison`.
    - Update the `test_comparison_params_path` fixture in
      `tests/controller_comparison_tests/conftest.py` to reflect the
      new location of the test comparison params YAML file.
    
    This commit improves the controller comparison integration test
    by using a simplified data-driven MPC controller that requires
    fewer iteration steps for initialization. Additionally, it improves
    the controller comparison test system by adding comparison-specific
    test files.

--- tests/controller_comparison_tests/conftest.py
+++ tests/controller_comparison_tests/conftest.py
@@ -7,5 +7,5 @@ import pytest
 def test_comparison_params_path() -> str:
     return os.path.join(
         os.path.dirname(__file__),
-        "config/test_controller_comparison_config.yaml",
+        "config/comparison/test_controller_comparison_config.yaml",
     )
