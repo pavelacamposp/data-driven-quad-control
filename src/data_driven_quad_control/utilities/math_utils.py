2240fb1e288a8c1ad2526b56d9c82571f9e63887
Pável A. Campos-Peña <pcamposp@uni.pe>
2025-06-30 06:52:36 -0500

    Fix static type check for updated PyTorch version
    Update `gs_rand_float` function in `math_utils.py` to align with
    the `torch.rand` signature from the updated PyTorch version.

--- src/data_driven_quad_control/utilities/math_utils.py
+++ src/data_driven_quad_control/utilities/math_utils.py
@@ -4,10 +4,10 @@ import torch
 def gs_rand_float(
     lower: float,
     upper: float,
-    shape: int | tuple[int, ...],
+    shape: tuple[int, ...],
     device: torch.device,
 ) -> torch.Tensor:
-    return (upper - lower) * torch.rand(size=shape, device=device) + lower
+    return (upper - lower) * torch.rand(*shape, device=device) + lower
 
 
 def linear_interpolate(
